2b237353bff59a0675443e0e9111a1f2b41c2c90
zvirus <zvirus@ya.ru>
2015-06-08 17:53:12 +0300

    Update send.php
    Добавлены Украинские сервисы

--- lib/send.php
+++ lib/send.php
@@ -19,9 +19,9 @@ $sms['id']  = '';
 $sms['key'] = '';
 $sms['log'] = '';
 $sms['pss'] = '';
-$sms['frm'] = 'callme'; // добавьте новую подпись в смс-шлюзе и дождитесь подтверждения
-$sms['num'] = '';  			// ваш номер в международном формате без "+"
-$sms['prv'] = 'sms.ru'; // на выбор: sms.ru, infosmska.ru, bytehand.com, sms-sending.ru, smsaero.ru
+$sms["frm"] = "callme"; // добавьте новую подпись в смс-шлюзе и дождитесь апрува. Для sms-fly.com, sms-sms.com.ua по умолчанию используется АИ SMS
+$sms["num"] = ""; // ваш номер в формате без + (79218886622, 380501234567)
+$sms["prv"] = "sms-sms.com.ua"; // на выбор: sms.ru, infosmska.ru, bytehand.com, sms-sending.ru, smsaero.ru, sms-fly.com, sms-sms.com.ua
 
 function uc ($s) {
 	return urlencode($s);
@@ -32,14 +32,92 @@ function gf ($s) {
 	if (strlen($s) > 1) return $s;
 }
 
-function sendSMS ($to, $msg) {
+function sendSMS ($to = "", $msg){
 	global $sms;
-	$u['sms.ru'] = "sms.ru/sms/send?api_id=".uc($sms["key"])."&to=".uc($sms["num"])."&text=".uc($msg);
-	$u['bytehand.com'] = "bytehand.com:3800/send?id=".uc($sms["id"])."&key=".uc($sms["key"])."&to=".uc($sms["num"])."&partner=callme&from=".uc($sms["frm"])."&text=".uc($msg);
-	$u['sms-sending.ru'] = "lcab.sms-sending.ru/lcabApi/sendSms.php?login=".uc($sms["log"])."&password=".uc($sms["pss"])."&txt=".uc($msg)."&to=".uc($sms["num"]);
-	$u['infosmska.ru'] = "api.infosmska.ru/interfaces/SendMessages.ashx?login=".uc($sms['log'])."&pwd=".uc($sms["pss"])."&sender=SMS&phones=".uc($sms["num"])."&message=".uc($msg);
-	$u['smsaero.ru'] = "gate.smsaero.ru/send/?user=".uc($sms["log"])."&password=".md5 (uc($sms["pss"]))."&to=".uc($sms["num"])."&text=".uc($msg)."&from=".uc($sms["frm"]);
-	@$r = file_get_contents("http://".$u[$sms["prv"]]);	
+	
+	# исправляем косяки автора. Он не передает номер телефона в параметре $to
+	$to = (empty($to)) ? $sms["num"] : $to; 
+	if($sms["prv"] == 'sms-fly.com' || $sms["prv"] == 'sms-sms.com.ua')
+	{
+		$description = htmlspecialchars('SMS from Buyme 1.4');
+		# Подменим общее Альфанумерическое имя на доступное на этом сервисе (SMS) по умолчанию всем клиентам
+		# В случае индивидуального Альфаимени используется из конфига
+		$sms["frm"] = ($sms["frm"] == "callme") ? "SMS" : $sms["frm"];
+
+		# проверка на доступность CURL
+		if (function_exists('curl_init')) 
+		{
+			# экранируем текст
+			#$msg = htmlspecialchars($msg);
+			$myXML 	 = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+			$myXML 	.= "<request>";
+			$myXML 	.= "<operation>SENDSMS</operation>";
+			$myXML 	.= '		<message start_time="AUTO" end_time="AUTO" livetime="5" rate="120" desc="'.$description.'" source="'.$sms["frm"].'">'."\n";
+			$myXML 	.= "		<body>".$msg."</body>";
+			$myXML 	.= "		<recipient>".$to."</recipient>";
+			$myXML 	.=  "</message>";
+			$myXML 	.= "</request>";
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_USERPWD , $sms["log"].':'.$sms["pss"]);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_URL, 'http://'.$sms["prv"].'/api/api.php');
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: text/xml", "Accept: text/xml"));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $myXML);
+			$response = curl_exec($ch); // тут содержится ответ от сервера. Можно распарсить и вернуть ошибку, если необходимо
+			curl_close($ch);
+		} 
+		else 
+		{
+			# пробуем отправить через сокеты
+			$line = array();  
+			$is_data = false;
+			$fp = fsockopen($sms["prv"], 80, $errno, $errstr, 30); 
+			if (!$fp) 
+			{ 
+				echo "$errstr ($errno)<br />\n";  // эту ошибку при отправке тоже бы обработать
+			}
+			else  
+			{  
+				$data 	  	 = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+				$data 	  	.= "<request>";
+				$data 	  	.= "<operation>SENDSMS</operation>";
+				$data 	  	.= '		<message start_time="AUTO" end_time="AUTO" livetime="3" rate="120" desc="'.$description.'" source="'.$sms["frm"].'">'."\n";
+				$data 	  	.= "		<body>".$msg."</body>";
+				$data 	  	.= "		<recipient>".$to."</recipient>";
+				$data 	  	.=  "</message>";
+				$data 	  	.= "</request>";				
+				
+				$headers = "POST /api/api.php HTTP/1.1\r\n";  
+				$headers .= "Host: ".$sms["prv"]."\r\n";  
+				$headers .= 'Authorization: Basic '.base64_encode($sms["log"].':'.$sms["pss"])."\r\n";           
+				$headers .= "Content-type: text/xml\r\n";  
+				$headers .= "Content-Length: ".strlen($data)."\r\n\r\n";  
+				fwrite($fp, $headers.$data);  
+				while (!feof($fp))  
+				{  
+					$one_line = fgets($fp, 1024);
+					if($one_line == "\r\n") {
+						$is_data = ($is_data) ? false : true;
+					}
+					if($is_data) $line[] = $one_line; // тут содержится ответ от сервера. Можно распарсить и вернуть ошибку, если необходимо
+				} 
+				fclose($fp);  
+			}  
+		}
+	}
+	else
+	{
+		$u['sms.ru'] = "sms.ru/sms/send?api_id=".uc($sms["key"])."&to=".uc($sms["num"])."&text=".uc($msg);
+		$u['bytehand.com'] = "bytehand.com:3800/send?id=".uc($sms["id"])."&key=".uc($sms["key"])."&to=".uc($sms["num"])."&partner=callme&from=".uc($sms["frm"])."&text=".uc($msg);
+		$u['sms-sending.ru'] = "lcab.sms-sending.ru/lcabApi/sendSms.php?login=".uc($sms["log"])."&password=".uc($sms["pss"])."&txt=".uc($msg)."&to=".uc($sms["num"]);
+		$u['infosmska.ru'] = "api.infosmska.ru/interfaces/SendMessages.ashx?login=".uc($sms['log'])."&pwd=".uc($sms["pss"])."&sender=SMS&phones=".uc($sms["num"])."&message=".uc($msg);
+		$u['smsaero.ru'] = "gate.smsaero.ru/send/?user=".uc($sms["log"])."&password=".md5 (uc($sms["pss"]))."&to=".uc($sms["num"])."&text=".uc($msg)."&from=".uc($sms["frm"]);
+	
+		@$r = file_get_contents("http://".$u[$sms["prv"]]);	
+	}
+	
 }
 
 function translit ($str) { // translit by programmerz.ru
